49dda7250e51578b8d02b2b6c1e19b5745e5cb4e
jcalhinho <lamaprod93@gmail.com>
2023-06-05 00:57:56 +0200

    copy site react app

---
+++ pages/data.tsx
@@ -0,0 +1,368 @@
+import pic from "./pix/01-THUMB_MrPaon.jpg";
+import pic1 from "./pix/02-THUMB_George Orwell.jpg";
+import pic2 from "./pix/04-THUMB_Clipse.jpg";
+import pic3 from "./pix/04-THUMB_Mon fils.jpg";
+import pic4 from "./pix/05-THUMB_edition BMR.jpg";
+import pic5 from "./pix/06-THUMB_Universite de Paris.jpg";
+import pic6 from "./pix/07-THUMB_Soli Solol Soli.jpg";
+import pic7 from "./pix/08-THUMB_Relyon.nous.jpg";
+import pic8 from "./pix/09-THUMB_Hurikat.jpg";
+import pic9 from "./pix/10-THUMB_Arsene Lupiupn.jpg";
+import pic10 from "./pix/11-THUMB_EUX IMPRO.jpg";
+import pic11 from "./pix/12-THUMB_HMpartners.jpg";
+import pic12 from "./pix/13-THUMB_Livre de poche jeunesse.jpg";
+import pic13 from "./pix/14-THUMB_Mairie de Paaris.jpg";
+import pic14 from "./pix/15-THUMB_Casalib.jpg";
+import pic15 from "./pix/16-THUMB_off jazz.jpg";
+import pic16 from "./pix/17-THUMB_Athemium.jpg";
+import pic17 from "./pix/18-THUMB_Biarritz.jpg";
+import pic18 from "./pix/19-THUMB_Smatch.jpg";
+import pic19 from "./pix/20-THUMB_Pelicafe.jpg";
+
+
+
+
+
+
+
+
+import paon from "./01-MR PAON/MR PAON_01.png";
+import paon1 from "./01-MR PAON/MR PAON_02.png";
+import paon2 from "./01-MR PAON/MR PAON_03.png";
+import paon3 from "./01-MR PAON/MR PAON_04.png";
+import paon4 from "./01-MR PAON/MR PAON_05.png";
+import paon5 from "./01-MR PAON/MR PAON_06.png";
+
+import orwell from "./02-GEORGE ORWELL/GEORGE ORWELL_01.jpg";
+import orwell1 from "./02-GEORGE ORWELL/GEORGE ORWELL_02.jpg";
+import orwell2 from "./02-GEORGE ORWELL/GEORGE ORWELL_03.jpg";
+
+import clipse from "./03-CLIPSE/CLIPSE_01.jpg";
+import clipse1 from "./03-CLIPSE/CLIPSE_02.jpg";
+import clipse2 from "./03-CLIPSE/CLIPSE_03.jpg";
+import clipse3 from "./03-CLIPSE/CLIPSE_04.jpg";
+import clipse4 from "./03-CLIPSE/CLIPSE_05.jpg";
+import clipse5 from "./03-CLIPSE/CLIPSE_06.jpg";
+import clipse6 from "./03-CLIPSE/CLIPSE_07.jpg";
+import clipse7 from "./03-CLIPSE/CLIPSE_08.jpg";
+import clipse8 from "./03-CLIPSE/CLIPSE_09.jpg";
+
+import monfils from "./04-MON FILS/MON-FILS-01.jpg";
+import monfils1 from "./04-MON FILS/MON-FILS-02.jpg";
+import monfils2 from "./04-MON FILS/MON-FILS-03.jpg";
+import monfils3 from "./04-MON FILS/MON-FILS-04.jpg";
+
+import bmr from "./05-BMR EDITION/BMR EDITION_01.png";
+import bmr1 from "./05-BMR EDITION/BMR EDITION_02.png";
+import bmr2 from "./05-BMR EDITION/BMR EDITION_03.png";
+import bmr3 from "./05-BMR EDITION/BMR EDITION_04.png";
+import bmr4 from "./05-BMR EDITION/BMR EDITION_05.png";
+import bmr5 from "./05-BMR EDITION/BMR EDITION_06.jpg";
+import bmr6 from "./05-BMR EDITION/BMR EDITION_07.png";
+import bmr7 from "./05-BMR EDITION/BMR EDITION_08.png";
+
+import issa from "./06-ISSA_UNIVERSITE DE PARIS/ISSA_UNIVERSITE DE PARIS_01.jpg";
+import issa1 from "./06-ISSA_UNIVERSITE DE PARIS/ISSA_UNIVERSITE DE PARIS_02.jpg";
+import issa2 from "./06-ISSA_UNIVERSITE DE PARIS/ISSA_UNIVERSITE DE PARIS_03.jpg";
+import issa3 from "./06-ISSA_UNIVERSITE DE PARIS/ISSA_UNIVERSITE DE PARIS_04.jpg";
+import issa4 from "./06-ISSA_UNIVERSITE DE PARIS/ISSA_UNIVERSITE DE PARIS_05.jpg";
+import issa5 from "./06-ISSA_UNIVERSITE DE PARIS/ISSA_UNIVERSITE DE PARIS_06.jpg";
+
+import soli from "./07-SOLI SOL SOLI/SOLI SOL SOLI_01.jpg";
+import soli1 from "./07-SOLI SOL SOLI/SOLI SOL SOLI_02.jpg";
+import soli2 from "./07-SOLI SOL SOLI/SOLI SOL SOLI_03.jpg";
+import soli3 from "./07-SOLI SOL SOLI/SOLI SOL SOLI_04.jpg";
+import soli4 from "./07-SOLI SOL SOLI/SOLI SOL SOLI_05.jpg";
+
+import relyon from "./08-RELYON NOUS/RELYON NOUS_01.jpg";
+import relyon1 from "./08-RELYON NOUS/RELYON NOUS_02.jpg";
+import relyon2 from "./08-RELYON NOUS/RELYON NOUS_03.jpg";
+import relyon3 from "./08-RELYON NOUS/RELYON NOUS_04.jpg";
+import relyon4 from "./08-RELYON NOUS/RELYON NOUS_05.jpg";
+import relyon5 from "./08-RELYON NOUS/RELYON NOUS_06.jpg";
+import relyon6 from "./08-RELYON NOUS/RELYON NOUS_07.jpg";
+import relyon7 from "./08-RELYON NOUS/RELYON NOUS_08.jpg";
+import relyon8 from "./08-RELYON NOUS/RELYON NOUS_09.jpg";
+
+import hurikat from "./09-HURIKAT/HURIKAT_01.jpg";
+import hurikat1 from "./09-HURIKAT/HURIKAT_02.jpg";
+import hurikat2 from "./09-HURIKAT/HURIKAT_03.jpg";
+import hurikat3 from "./09-HURIKAT/HURIKAT_04.jpg";
+import hurikat4 from "./09-HURIKAT/HURIKAT_05.jpg";
+import hurikat5 from "./09-HURIKAT/HURIKAT_06.jpg";
+
+import arsene from "./10-ARSENE LUPIN/ARSENE LUPIN_01.jpg";
+import arsene1 from "./10-ARSENE LUPIN/ARSENE LUPIN_02.jpg";
+import arsene2 from "./10-ARSENE LUPIN/ARSENE LUPIN_03.jpg";
+import arsene3 from "./10-ARSENE LUPIN/ARSENE LUPIN_04.jpg";
+import arsene4 from "./10-ARSENE LUPIN/ARSENE LUPIN_05.jpg";
+
+import eux from "./11-EUX IMPRO/EUX impro_01.jpg";
+import eux1 from "./11-EUX IMPRO/EUX impro_02.jpg";
+import eux2 from "./11-EUX IMPRO/EUX impro_03.jpg";
+import eux3 from "./11-EUX IMPRO/EUX impro_04.jpg";
+
+import hm from "./12-HM PARTNERS/HM PARTNERS_01.jpg";
+import hm1 from "./12-HM PARTNERS/HM PARTNERS_02.jpg";
+import hm2 from "./12-HM PARTNERS/HM PARTNERS_03.jpg";
+import hm3 from "./12-HM PARTNERS/HM PARTNERS_04.jpg";
+import hm4 from "./12-HM PARTNERS/HM PARTNERS_05.jpg";
+
+import livre from "./13-LIVRE DE POCHE JEUNESSE/LIVRE DE POCHE JEUNESSE_01.jpg";
+import livre1 from "./13-LIVRE DE POCHE JEUNESSE/LIVRE DE POCHE JEUNESSE_02.jpg";
+import livre2 from "./13-LIVRE DE POCHE JEUNESSE/LIVRE DE POCHE JEUNESSE_03.jpg";
+import livre3 from "./13-LIVRE DE POCHE JEUNESSE/LIVRE DE POCHE JEUNESSE_04.jpg";
+import livre4 from "./13-LIVRE DE POCHE JEUNESSE/LIVRE DE POCHE JEUNESSE_05.jpg";
+import livre5 from "./13-LIVRE DE POCHE JEUNESSE/LIVRE DE POCHE JEUNESSE_06.jpg";
+import livre6 from "./13-LIVRE DE POCHE JEUNESSE/LIVRE DE POCHE JEUNESSE_07.jpg";
+import livre7 from "./13-LIVRE DE POCHE JEUNESSE/LIVRE DE POCHE JEUNESSE_08.jpg";
+
+import mairie from "./14-MAIRIE DE PARIS/MAIRIE DE PARIS_01.jpg";
+import mairie1 from "./14-MAIRIE DE PARIS/MAIRIE DE PARIS_02.jpg";
+import mairie2 from "./14-MAIRIE DE PARIS/MAIRIE DE PARIS_03.jpg";
+import mairie3 from "./14-MAIRIE DE PARIS/MAIRIE DE PARIS_04.jpg";
+
+import casa from "./15-CASALIB/CASALIB_01.jpg";
+import casa1 from "./15-CASALIB/CASALIB_02.jpg";
+import casa2 from "./15-CASALIB/CASALIB_03.jpg";
+import casa3 from "./15-CASALIB/CASALIB_04.jpg";
+import casa4 from "./15-CASALIB/CASALIB_05.jpg";
+import casa5 from "./15-CASALIB/CASALIB_06.jpg";
+import casa6 from "./15-CASALIB/CASALIB_07.jpg";
+import casa7 from "./15-CASALIB/CASALIB_08.jpg";
+
+import jazz from "./16-OFF JAZZ/OFF JAZZ_01.jpg";
+import jazz1 from "./16-OFF JAZZ/OFF JAZZ_02.jpg";
+import jazz2 from "./16-OFF JAZZ/OFF JAZZ_03.jpg";
+import jazz3 from "./16-OFF JAZZ/OFF JAZZ_04.jpg";
+import jazz4 from "./16-OFF JAZZ/OFF JAZZ_05.jpg";
+import jazz5 from "./16-OFF JAZZ/OFF JAZZ_06.jpg";
+import jazz6 from "./16-OFF JAZZ/OFF JAZZ_07.jpg";
+import jazz7 from "./16-OFF JAZZ/OFF JAZZ_08.jpg";
+
+import athe from "./17-ATHEMIUM/ATHEMIUM_01.jpg";
+import athe1 from "./17-ATHEMIUM/ATHEMIUM_02.jpg";
+import athe2 from "./17-ATHEMIUM/ATHEMIUM_03.jpg";
+import athe3 from "./17-ATHEMIUM/ATHEMIUM_04.jpg";
+import athe4 from "./17-ATHEMIUM/ATHEMIUM_05.jpg";
+
+import bia from "./18-BIARRITZ TOURISME/BIARRITZ TOURISME_01.jpg";
+import bia1 from "./18-BIARRITZ TOURISME/BIARRITZ TOURISME_02.jpg";
+import bia2 from "./18-BIARRITZ TOURISME/BIARRITZ TOURISME_03.jpg";
+import bia3 from "./18-BIARRITZ TOURISME/BIARRITZ TOURISME_04.jpg";
+import bia4 from "./18-BIARRITZ TOURISME/BIARRITZ TOURISME_05.jpg";
+import bia5 from "./18-BIARRITZ TOURISME/BIARRITZ TOURISME_06.jpg";
+import bia6 from "./18-BIARRITZ TOURISME/BIARRITZ TOURISME_07.jpg";
+import bia7 from "./18-BIARRITZ TOURISME/BIARRITZ TOURISME_08.jpg";
+
+import smatch from "./19-SMATCH/PELICAFE_01.jpg";
+import smatch1 from "./19-SMATCH/PELICAFE_02.jpg";
+import smatch2 from "./19-SMATCH/PELICAFE_03.jpg";
+import smatch3 from "./19-SMATCH/PELICAFE_04.jpg";
+import smatch4 from "./19-SMATCH/PELICAFE_05.jpg";
+import smatch5 from "./19-SMATCH/PELICAFE_06.jpg";
+import smatch6 from "./19-SMATCH/PELICAFE_07.jpg";
+import smatch7 from "./19-SMATCH/PELICAFE_08.jpg";
+import smatch8 from "./19-SMATCH/PELICAFE_09.jpg";
+import smatch9 from "./19-SMATCH/PELICAFE_10.jpg";
+
+import peli from "./20-PELICAFE/PELICAFE_01.jpg";
+import peli1 from "./20-PELICAFE/PELICAFE_02.jpg";
+import peli2 from "./20-PELICAFE/PELICAFE_03.jpg";
+import peli3 from "./20-PELICAFE/PELICAFE_04.jpg";
+import peli4 from "./20-PELICAFE/PELICAFE_05.jpg";
+import peli5 from "./20-PELICAFE/PELICAFE_06.jpg";
+import peli6 from "./20-PELICAFE/PELICAFE_07.jpg";
+
+export const Data = [
+    {
+        id: 1,
+        type: "Identité de marque",
+        titre: "MONSIEUR PAON",
+        subtitle: "Bière Craft",
+        desc: "Identité de marque et étiquette créées pour cette brasserie Lilloise composée de passionnés,appliqués à développer et rechercher des goûts uniques dans un souci de précision et desavoir-faire artisanal.",
+        pics: [paon, paon1, paon2, paon3, paon4, paon5],
+        vignette: pic,
+    },
+    {
+        id: 2,
+        type: "Edition",
+        titre: "GEORGE ORWELL",
+        subtitle: "1984 / La ferme des animaux",
+        desc: "Illustrations réalisées pour la réédition de ces deux monuments de la littérature, édités au LIVRE DE POCHE JEUNESSE.",
+        pics: [orwell, orwell1, orwell2],
+        vignette: pic1,
+    },
+    {
+        id: 3,
+        type: "Identité de marque",
+        titre: "CLIPSE",
+        subtitle: "Link People",
+        credit: "Design UX/UI en collaboration avec Elie Monge",
+        desc: "Identité de marque créée pour cette application dédiée au bouche à oreille, au partage de connaissances et à l’entraide entre utilisateurs.Partager ses contacts professionnels et amicaux et créer des imbrications fructueuses.",
+        pics: [clipse, clipse1, clipse2, clipse3, clipse4, clipse5, clipse6, clipse7, clipse8,
+        ],
+        vignette: pic2,
+    },
+    {
+        id: 4,
+        type: "Evénement culturel",
+        titre: "MON FILS",
+        subtitle: "Réparer une vie en une nuit",
+        desc: "Affiche réalisée pour la pièce de théâtre MON FILS d’Erwan Szejnok. Huis clos onirique entre un homme et son père, survivant de la Shoah.",
+        pics: [monfils, monfils1, monfils2, monfils3],
+        vignette: pic3,
+    },
+    {
+        id: 5,
+        type: "Identité de marque",
+        titre: "BMR",
+        subtitle: "La romance sans complexe",
+        desc: "Le label de romance adulte des éditions Hachette Livre. Une identité singulière et désinhibée pour une collection de littérature sulfureuse.",
+        pics: [bmr, bmr1, bmr2, bmr3, bmr4, bmr5, bmr6, bmr7],
+        vignette: pic4,
+    },
+    {
+        id: 6,
+        type: "Evénement culturel",
+        titre: "ISSA",
+        subtitle: "Université Paris Cité",
+        desc: "Conception de l’identité et illustration du cinquantième congrès mondial de sociologie du sport organisé par International Sociology of Sport Association à l’Université Paris Cité-UFR Staps.",
+        pics: [issa, issa1, issa2, issa3, issa4, issa5],
+        vignette: pic5,
+    },
+    {
+        id: 7,
+        type: "Identité de marque",
+        titre: "SOLI SOL SOLI",
+        subtitle: "Vin vivant",
+        credit: "«Le sol, les vignes, le soleil et nos mains.»",
+        desc: "Telle est la devise de ces vignerons qui souhaitaient une identité illustrant leur rapport humble à la terre et au nectar qu’ils en extraient.",
+        pics: [soli, soli1, soli2, soli3, soli4],
+        vignette: pic6,
+    },
+    {
+        id: 8,
+        type: "Evénement culturel",
+        titre: "RE LYON NOUS",
+        subtitle: "ville de Lyon",
+        credit: "Direction artistique réalisée pour le compte de l'agence Extra.",
+        desc: "Conception de l’identité et illustration pour cette manifestation annuelle organisée par la ville de Lyon. Jeu de piste ludique et urbain favorisant le lien intergénérationnel et la découverte de la ville.",
+        pics: [relyon, relyon1, relyon2, relyon3, relyon4, relyon5, relyon6, relyon7, relyon8,],
+        vignette: pic7,
+    },
+    {
+        id: 9,
+        type: "Identité de marque",
+        titre: "HURIKAT",
+        subtitle: "Social waiting",
+        credit: "Direction artistique réalisée pour le compte de Generous Branding",
+        desc: "Identité de marque créée pour un nouveau service communautaire permettant d’optimiser vos temps d’attente dans tous les lieux du quotidien. Tel le suricate soyez aux aguets et prévenez votre communauté.",
+        pics: [hurikat, hurikat1, hurikat2, hurikat3, hurikat4, hurikat5],
+        vignette: pic8,
+    },
+    {
+        id: 10,
+        type: "Edition",
+        titre: "ARSÈNE LUPIN",
+        subtitle: "Maurice Leblanc",
+        desc: "Réédition des aventures du plus célèbre des cambrioleurs, par LE LIVRE DE POCHE JEUNESSE. Création d’illustrations mettant en valeur le caractère iconique du personnage emblématique de Maurice Leblanc.",
+        pics: [arsene, arsene1, arsene2, arsene3, arsene4],
+        vignette: pic9,
+    },
+    {
+        id: 11,
+        type: "Evénement culturel",
+        titre: "Compagnie LES EUX",
+        subtitle: "Impro Feydeau",
+        desc: "Affiche de théâtre réalisée pour la compagnie d’improvisation LES EUX. Un spectacle virevoltant, avec son lot de maris désœuvrés, de quiproquos improbables et de portes qui claquent.",
+        pics: [eux, eux1, eux2, eux3],
+        vignette: pic10,
+    },
+    {
+        id: 12,
+        type: "Identité de marque",
+        titre: "HM PARTNERS",
+        credit: "Direction artistique réalisée pour le compte du studio Contreform’",
+        desc: "Identité visuelle réalisée pour une société spécialisée dans la mise en place d’équipements et de solutions en infogérance, réseaux, télécom et impression.",
+        pics: [hm, hm1, hm2, hm3, hm4],
+        vignette: pic11,
+    },
+    {
+        id: 13,
+        type: "Edition",
+        titre: "LE LIVRE DE POCHE JEUNESSE",
+        subtitle: "Les 40 ans",
+        desc: "Création d’une campagne de communication mise en place pour inviter les jeunes lecteurs et les libraires à découvrir les secrets, les actus et les coulisses du LIVRE DE POCHE JEUNESSE à l’occasion de ses 40 ans.",
+        pics: [livre, livre1, livre2, livre3, livre4, livre5, livre6, livre7],
+        vignette: pic12,
+    },
+    {
+        id: 14,
+        type: "Evénement culturel",
+        titre: "MAIRIE DE PARIS",
+        subtitle: "Pour l’autonomie des jeunes",
+        desc: "Affiche réalisée pour une campagne de la mairie de Paris mettant en avant toutes les nouvelles actions engagées par la collectivité en faveur des jeunes.",
+        pics: [mairie, mairie1, mairie2, mairie3],
+        vignette: pic13,
+    },
+    {
+        id: 15,
+        type: "Identité de marque",
+        titre: "CASALIB",
+        subtitle: "Extension de maison",
+        credit: "Direction artistique réalisée pour le compte du studio Contreform'",
+        desc: "Identité créée pour cette société proposant des solutions simples et sur mesure à tous ceux qui désirent agrandir leur maison sans les tracas des travaux et chercher un architecte. Un vrai jeu d’enfant !",
+        pics: [casa, casa1, casa2, casa3, casa4, casa5, casa6, casa7],
+        vignette: pic14,
+    },
+    {
+        id: 16,
+        type: "Evénement culturel",
+        titre: "L’OFF JAZZ",
+        subtitle: "Festival de Montréal",
+        desc: "Conception de l’identité visuelle et des illustrations de la 14eme édition du festival indépendant du jazz à Montréal. Un événement profondément humain et festif mettant en avant le jazz contemporain québécois et du monde entier.",
+        pics: [jazz, jazz1, jazz2, jazz3, jazz4, jazz5, jazz6, jazz7],
+        vignette: pic15,
+    },
+    {
+        id: 17,
+        type: "Identité de marque",
+        titre: "ATHEMIUM",
+        subtitle: "Life automation",
+        credit: "Direction artistique réalisé pour le compte du studio Nouvelle Etiquette",
+        desc: "Société de domotique développant des applications pour simplifier la vie de ses utilisateurs. Création d’une identité visuelle en référence à la diffusion de son action protectrice.",
+        pics: [athe, athe1, athe2, athe3, athe4],
+        vignette: pic16,
+    },
+    {
+        id: 18,
+        type: "Edition",
+        titre: "BIARRITZ",
+        subtitle: "Guide touristique",
+        credit: "Direction de création réalisée en collaboration avec Xavier Pompelle et Marion Duci",
+        desc: "Charte graphique réalisée pour mettre en valeur l’élégance, le dynamisme et la modernité de la ville de Biarritz qui offre à ses visiteurs des événements variés et authentiques tout au long de l’année.",
+        pics: [bia, bia1, bia2, bia3, bia4, bia5, bia6, bia7],
+        vignette: pic17,
+    },
+    {
+        id: 19,
+        type: "Identité de marque",
+        titre: "SMATCH",
+        subtitle: "Supermarché",
+        credit: "Direction artistique réalisée pour le compte de Saguez and Partners",
+        desc: "Identité mettant en avant le foisonnement d’offres que proposent les supermarchés SMATCH implantés en Belgique. La création d’une mascotte au bec rebondi illustre la capacité du consommateur à se faire toujours + plaisir.",
+        pics: [smatch, smatch1, smatch2, smatch3, smatch4, smatch5, smatch6, smatch7, smatch8, smatch9],
+        vignette: pic18,
+    },
+    {
+        id: 20,
+        type: "Identité de marque",
+        titre: "PELICAFE",
+        subtitle: "Restaurant",
+        credit: "Direction artistique réalisée pour le compte de Saguez and Partners",
+        desc: "Pélicafé est un lieu de détente et de restauration, annexé au supermarché SMATCH.L’intégralité de l’image de marque, de l’aménagement intérieur à la papeterie, respire la bonhomie et la sympathie de la mascotte SMATCH.",
+        pics: [peli, peli1, peli2, peli3, peli4, peli5, peli6],
+        vignette: pic19,
+    },
+];
